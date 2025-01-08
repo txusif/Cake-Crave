cce8bf45f1303b68f4c05e1c2d8d5bcdaae3c957
txusif <txusif@gmail.com>
2025-01-08 12:31:30 +0530

    removed red bg color from formcover

--- src/ui/FormCover.tsx
+++ src/ui/FormCover.tsx
@@ -1,7 +1,7 @@
 export default function FormCover({ bannerImage }: { bannerImage: string }) {
   return (
-    <div className="box-content hidden bg-red-500 sm:block">
-      <img className="h-full object-cover" src={bannerImage} alt="banner" />
+    <div className="box-content hidden sm:block">
+      <img className="invisible h-full" src={bannerImage} alt="banner" />
     </div>
   );
 }
