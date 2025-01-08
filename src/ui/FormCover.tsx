8f840977c03cd59346f649cf8a7303421baecdb0
txusif <txusif@gmail.com>
2025-01-08 12:39:25 +0530

    fixed auth page form cover

--- src/ui/FormCover.tsx
+++ src/ui/FormCover.tsx
@@ -1,7 +1,7 @@
 export default function FormCover({ bannerImage }: { bannerImage: string }) {
   return (
     <div className="box-content hidden sm:block">
-      <img className="invisible h-full" src={bannerImage} alt="banner" />
+      <img className=" h-full" src={bannerImage} alt="banner" />
     </div>
   );
 }
